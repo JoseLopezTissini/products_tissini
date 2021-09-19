76ccc863803fb912e3025a8d18f01ef74359cac9
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2021-09-19 17:41:20 -0500

    rendered document

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,29 +1,39 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { ReactElement, useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { NextPage } from 'next';
+import axios from 'axios';
 
 import ProductItem from '@components/products/ProductItem';
 import MainLayout from '@layouts/main-layout';
 
-import { RootState } from '@core/store/store';
-import { getProducts } from '@core/store/products/products.action';
-import { Product } from '@core/models/products.model';
+import { setPrices, setProducts } from '@core/store/products/products.action';
+import { Product, ProductPrice } from '@core/models/products.model';
 import { addProduct } from '@core/store/cart/cart.action';
 
-const Products = () => {
-  const { products } = useSelector((state: RootState) => state.products);
+const Products: NextPage & {
+  getLayout: (page: ReactElement) => void;
+} = ({ products }: any) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
+    const productsPrices: ProductPrice[] = products.map((product: Product) => ({
+      productid: product.id,
+      price: product.variants[0] ? +product.variants[0].price : 0,
+    }));
+    dispatch(setProducts(products));
+    dispatch(setPrices(productsPrices));
+  }, [dispatch, products]);
 
-  const handleOnAdd = useCallback((product: Product) => {
-    dispatch(addProduct(product));
-  }, [dispatch]);
+  const handleOnAdd = useCallback(
+    (product: Product) => {
+      dispatch(addProduct(product));
+    },
+    [dispatch]
+  );
 
   return (
     <>
-      {products.map((product) => (
+      {products.map((product: Product) => (
         <ProductItem
           product={product}
           handleOnAdd={handleOnAdd}
@@ -36,6 +46,16 @@ const Products = () => {
 
 Products.getLayout = (page: ReactElement) => {
   return <MainLayout>{page}</MainLayout>;
+};
+
+Products.getInitialProps = async () => {
+  const response = await axios.get(
+    'https://l8.tissini.dev/api/v3/categories/1/products'
+  );
+  const { products } = await response.data;
+  return {
+    products,
+  };
 };
 
 export default Products;
